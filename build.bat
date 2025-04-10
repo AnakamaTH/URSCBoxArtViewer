76fcfab7b63ab07ded755117209c7e28c23c6909
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-04-11 00:21:20 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+title PY EXE BUILDER
 echo Building URSC Box Art Viewer...
 pip install pyinstaller pillow >nul
 pyinstaller URSC_BoxArt.py --onefile --version-file=version.txt --icon=sonic.ico 
